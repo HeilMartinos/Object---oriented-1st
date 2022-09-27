1d1d6e188a492c516eac751ca0a3e806ffdd9af0
Marcin_Bator <marcinbator.ofc@gmail.com>
2022-09-27 11:46:12 +0200

    new

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 int main()
 {
+    int points = 0;
     Question q[2];
-    for (int i = 1; i <= 2; i++)
+    for (int i = 0; i < 2; i++)
     {
+        q[i].no = i + 1;
         q[i].load();
         q[i].print();
         q[i].check();
+        points += q[i].point;
     }
+    cout << "Zdobyles " << points << "pkt/2pkt." << endl;
 }
